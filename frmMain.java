2208be687d2f27061fb58818ed58089ede592244
Esma Krasniqi <44786311+esmakrasniqi@users.noreply.github.com>
2019-05-26 15:44:07 +0200

    Update frmMain.java

--- frmMain.java
+++ frmMain.java
@@ -1,147 +1,70 @@
 package projekti;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import java.awt.ScrollPane;
-import javax.swing.JTextPane;
-import java.awt.Color;
-import java.awt.Button;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.UIManager;
-import java.awt.TextField;
-import java.awt.List;
-import javax.swing.JToolBar;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.BoxLayout;
-import javax.swing.JDesktopPane;
-import javax.swing.JButton;
-import javax.swing.JProgressBar;
-import javax.swing.JRadioButtonMenuItem;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class frmMain extends JFrame {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_3;
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-	/**
-	 * Launch the application.
-	 */
+
+
+public class frmMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {      
+	GridPane pane = new GridPane();
+
+		pane.setAlignment(Pos.TOP_LEFT);
+		pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+		pane.setHgap(5.5);
+		pane.setVgap(15);
+
+		pane.add(new Label("Nisja:"), 0, 0);
+		pane.add(new TextField(), 1, 0);
+		pane.add(new Label("Destinacioni:"), 0, 1);
+		pane.add(new TextField(), 1, 1);		
+	    Button btnKerko = new Button("Kerko");
+
+	pane.add(btnKerko, 1, 2);
+	GridPane.setHalignment(btnKerko, HPos.RIGHT);
+
+	pane.add(new Label("Komente, sygjerime, ankesa:"), 0, 5);
+	pane.add(new Label("Emri:"), 0, 6);
+	pane.add(new TextField(), 1, 6);
+	
+	
+	
+	Scene scene = new Scene(pane);
+	primaryStage.setTitle("Stacioni i autobuseve"); 
+	primaryStage.setScene(scene); 
+	primaryStage.show(); 
+	}
+	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frmMain frame = new frmMain();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Application.launch(args);
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public frmMain() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 582, 390);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnKryefaqja = new JMenu("Kryefaqja");
-		menuBar.add(mnKryefaqja);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
-		mnKryefaqja.add(mntmNewMenuItem);
-		
-		JMenu mnLinjat = new JMenu("Linjat");
-		menuBar.add(mnLinjat);
-		
-		JMenu mnRezervoni = new JMenu("Rezervoni");
-		menuBar.add(mnRezervoni);
-		
-		JMenu mnNdihme = new JMenu("Ndihme");
-		menuBar.add(mnNdihme);
-		
-		JMenu mnNewMenu = new JMenu("Gjuha");
-		menuBar.add(mnNewMenu);
-		
-		JMenuItem mntmShqip = new JMenuItem("Shqip");
-		mnNewMenu.add(mntmShqip);
-		
-		JMenuItem mntmAnglisht = new JMenuItem("Anglisht");
-		mnNewMenu.add(mntmAnglisht);
-		
-		JMenu mnKycuni = new JMenu("Kycuni");
-		menuBar.add(mnKycuni);
-		
-		JMenuItem mntmLidhu = new JMenuItem("Lidhu");
-		mnKycuni.add(mntmLidhu);
-		
-		JMenuItem mntmRegjistrohu = new JMenuItem("Regjistrohu");
-		mnKycuni.add(mntmRegjistrohu);
-		getContentPane().setLayout(null);
-		
-		JLabel lblNisja = new JLabel("Nisja");
-		lblNisja.setBounds(37, 40, 64, 28);
-		getContentPane().add(lblNisja);
-		
-		textField = new JTextField();
-		textField.setBounds(145, 39, 115, 31);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblDestinacioni = new JLabel("Destinacioni");
-		lblDestinacioni.setBounds(37, 79, 85, 36);
-		getContentPane().add(lblDestinacioni);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(145, 81, 115, 28);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		JButton btnKerko = new JButton("Kerko");
-		btnKerko.setBounds(290, 64, 89, 23);
-		getContentPane().add(btnKerko);
-		
-		JLabel lblKomentesygjerimeAnkesa = new JLabel("Komente,sygjerime, ankesa");
-		lblKomentesygjerimeAnkesa.setBounds(10, 211, 217, 14);
-		getContentPane().add(lblKomentesygjerimeAnkesa);
-		
-		TextField textField_2 = new TextField();
-		textField_2.setBounds(10, 257, 397, 63);
-		getContentPane().add(textField_2);
-		
-		Button button = new Button("Dergo");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		button.setBounds(413, 231, 70, 22);
-		getContentPane().add(button);
-		
-		Button button_1 = new Button("Fshij");
-		button_1.setBounds(413, 264, 70, 22);
-		getContentPane().add(button_1);
-		
-		JLabel lblNewLabel = new JLabel("Emri:");
-		lblNewLabel.setBounds(10, 236, 46, 14);
-		getContentPane().add(lblNewLabel);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(37, 231, 139, 20);
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
-	}
+	
+	
+	
+	
 }
