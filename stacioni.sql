07ec95419f30d4627e48a4ce57e78c2c807b5b0b
Vetonshala <43969779+vetonnshala@users.noreply.github.com>
2019-05-28 10:54:26 +0200

    Update stacioni.sql

--- stacioni.sql
+++ stacioni.sql
@@ -19,11 +19,11 @@ Destinacioni varchar(50) not null ,
 Oranisjes time not null ,
 OraArritjes time not null ,
 Cmimi real not null ,
-Aid integer ,
-foreign key(Aid) references Autobusi(Aid));
+EmriKompanis varchar(30) ,
+Verifikmi boolean);
                          						
 
-show tables;
+
 create table Biletat (
 Bid integer auto_increment primary key not null unique ,
 Lid integer not null ,
@@ -31,17 +31,20 @@ Bemri varchar(40) not null ,
 Bmbiemri varchar(40) not null ,
 B_ora time not null ,
 NrBiletav integer not null ,
+Kthyese boolean,
 foreign key(Lid) references Linjat(Lid) on delete cascade);
 
 
 
-create table Administatori(
-Ad_id integer primary key  auto_increment not null ,
+create table Regjistrohu(
+User_id integer primary key  auto_increment not null ,
 Emri varchar(30) not null ,
 Mbiemri varchar(40) not null ,
-Username varchar(40) not null ,
 Email varchar(40) not null ,
+Username varchar(40) not null ,
 Password varchar(40) not null);
+
+insert into Regjistrohu(Emri,Mbiemri,Email,Username,Password) values ('Veton','Shala','vetonnshala@gmail.com','veton','1234');
 
 create table Komentet(
 Kid integer primary key auto_increment not null unique ,
