4cca804dcd07784e63769b38c5c21cad7eaa485b
Vetonshala <43969779+vetonnshala@users.noreply.github.com>
2019-06-02 23:10:25 +0200

    Update ShikoLinjat.java

--- ShikoLinjat.java
+++ ShikoLinjat.java
@@ -24,124 +24,143 @@ import javafx.stage.Stage;
 public class ShikoLinjat extends MainStacioni {
 	public  void start(Stage linjat)
 	{
+	   private TextField LidTxt = new TextField();
+	private TextField Vendi_nisjestxt = new TextField();
+	private TextField Destinacionitxt = new TextField();
+	private TextField Oranisjestxt = new TextField();
+	private TextField Oraarritjestxt = new TextField();
+	private TextField Cmimitxt = new TextField();
+	private TextField EmriKompanistxt = new TextField();
+	private TextField Verifikimitxt = new TextField();
+
 	
+//	// Buttons
+//	
+	private Button updateBtn = new Button("Update");
+	private Button deleteBtn = new Button("Delete");
+//	
 
 	       // Text scenetitle = new Text("Linjat e Autobuseve");
 	       // scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
 	        
 	        
 	        
-	        BorderPane title = new BorderPane();
-	       
-//	        title.setPadding(new Insets(10,10,10,10));
-//			title.getChildren().add(scenetitle);
+	      		GridPane boxpane1 = new GridPane();
+		
+//		boxpane1.addRow(0, new Label("ID: "), LidTxt);
+		boxpane1.addRow(1, new Label("VendiNis: "), Vendi_nisjestxt);
+		boxpane1.setAlignment(Pos.BOTTOM_LEFT);
+		boxpane1.addRow(2, new Label("Desti: "), Destinacionitxt);
+		boxpane1.addRow(3, new Label("OraN: "), Oranisjestxt);
+		
+		GridPane boxpane2 = new GridPane();
+		boxpane2.setAlignment(Pos.BOTTOM_CENTER);
+     	boxpane2.addRow(1, new Label("OraA: "), Oraarritjestxt);
+    	boxpane2.addRow(2, new Label("Cmimi: "), Cmimitxt);
+    	boxpane2.addRow(3, new Label("EmriK: "), EmriKompanistxt);
+    	
+    	GridPane boxpane3 = new GridPane();
+    	boxpane2.setAlignment(Pos.BOTTOM_RIGHT);
+    	boxpane2.addRow(1, new Label("Verifikimi"), Verifikimitxt);
+    	boxpane2.addRow(2, new Label(""), deleteBtn);
+    	boxpane2.addRow(3, new Label(" "), updateBtn);
+		
+		boxpane2.setHgap(10);
+		boxpane2.setVgap(10);
+		boxpane3.setHgap(10);
+		boxpane3.setVgap(10);
+		boxpane1.setHgap(10);
+		boxpane1.setVgap(10);
+		
+		updateBtn.setOnAction(e->{
+			//Linjattable.updateLinjattable(0, null, null, null, null, 0, null, false);
+		});
+
+		
+		HBox downPane = new HBox(15);
 	
-	
-		TableView<String> TabelaLinjave = new TableView<>();
-		
-		
-		 TableColumn<String,LinjatA>column1 = new TableColumn<>("Lid");
-		column1.setCellValueFactory(new PropertyValueFactory<>("Lid"));
-		column1.setPrefWidth(60);
-		
-		
-		TableColumn<String , LinjatA>column2 = new TableColumn<>("Vendi Nisjes");
-		column2.setCellValueFactory(new PropertyValueFactory<>("Vendi_nisjes"));
-				column2.setPrefWidth(130);
-		
-
-		TableColumn<String ,LinjatA>column3 = new TableColumn<>("Destinacioni");
-	     column3.setCellValueFactory(new PropertyValueFactory<>("Destinacioni"));
-				column3.setPrefWidth(100);
-
-		TableColumn<String , LinjatA>column4= new TableColumn<>("Ora Nisjes");
-		column4.setCellValueFactory(new PropertyValueFactory<>("Oranisjes "));
-				column4.setPrefWidth(100);
-
-		TableColumn<String , LinjatA>column5 = new TableColumn<>("Ora Arritjes");
-		column5.setCellValueFactory(new PropertyValueFactory<>("OraArritjes"));
-				column5.setPrefWidth(100);
-				
-		TableColumn<String , LinjatA>column6 = new TableColumn<>("Cmimi");
-		column6.setCellValueFactory(new PropertyValueFactory<>("Cmimi"));
-						column6.setPrefWidth(100); 
-						
-		TableColumn<String , LinjatA>column7 = new TableColumn<>("Emri Kompanis ");
-		column7.setCellValueFactory(new PropertyValueFactory<>("EmriKompanis"));
-		column7.setPrefWidth(100);
-								
-		TableColumn<String , LinjatA>column8 = new TableColumn<>("Verifikimi");
-		column8.setCellValueFactory(new PropertyValueFactory<>("Verifikimi"));
-				column7.setPrefWidth(100);
-							
-						
-								TabelaLinjave.getColumns().add(column1);
-								TabelaLinjave.getColumns().add(column2);
-								TabelaLinjave.getColumns().add(column3);
-								TabelaLinjave.getColumns().add(column4);
-								TabelaLinjave.getColumns().add(column5);
-								TabelaLinjave.getColumns().add(column6);
-								TabelaLinjave.getColumns().add(column7);
-								TabelaLinjave.getColumns().add(column8);
-		
-		
-								TabelaLinjave.setPrefWidth(650);
-								TabelaLinjave.setPrefHeight(40);
+		downPane.getChildren().addAll(boxpane1,boxpane2,boxpane3);
 		
 	
-		VBox linjatpane = new VBox();
-		linjatpane.getChildren().addAll(title,TabelaLinjave);
-		linjatpane.setPadding(new Insets(25,25,25,25)) ;
+		TableColumn<String, Linjattable> Linjac = new TableColumn<>("ID");
+		Linjac.setCellValueFactory(new PropertyValueFactory("Lid"));
+		Linjac.setPrefWidth(10);
+		
+		TableColumn<String, Linjattable> Vendinisjesc = new TableColumn<>("VendiNisjes");
+		 Vendinisjesc.setCellValueFactory(new PropertyValueFactory("Vendi_nisjes"));
+		 Vendinisjesc.setPrefWidth(65);
+		
+		TableColumn<String, Linjattable> Destinacionic = new TableColumn<>("Destinacioni");
+		 Destinacionic.setCellValueFactory(new PropertyValueFactory("Destinacioni"));
+		 Destinacionic.setPrefWidth(65);
+		
+		TableColumn<String, Linjattable> Oranisjesc = new TableColumn<>("OraNisjes");
+		Oranisjesc.setCellValueFactory(new PropertyValueFactory("Oranisjes"));
+		Oranisjesc.setPrefWidth(70);
+		
+		TableColumn<String, Linjattable> OraArritjesc = new TableColumn<>("OraArritjes");
+		OraArritjesc.setCellValueFactory(new PropertyValueFactory("OraArritjes"));
+		OraArritjesc.setPrefWidth(70);
+		
+		TableColumn<String, Linjattable> Cmimic = new TableColumn<>("Cmimi");
+		Cmimic.setCellValueFactory(new PropertyValueFactory("Cmimi"));
+		Cmimic.setPrefWidth(55);
+		
+		TableColumn<String, Linjattable> EmriKompanisc= new TableColumn<>("EmriKompanis");
+		EmriKompanisc.setCellValueFactory(new PropertyValueFactory("EmriKompanis"));
+		EmriKompanisc.setPrefWidth(100);
+		
+		TableColumn<String, Linjattable> Verifikmic = new TableColumn<>("Verifikimi");
+		Verifikmic.setCellValueFactory(new PropertyValueFactory("Verifikimi"));
+		Verifikmic.setPrefWidth(70);
+		
+		LinjattablesTable.getColumns().add(Linjac);
+		LinjattablesTable.getColumns().add(Vendinisjesc);
+		LinjattablesTable.getColumns().add(Destinacionic);
+		LinjattablesTable.getColumns().add(Oranisjesc);
+		LinjattablesTable.getColumns().add(	OraArritjesc);
+		LinjattablesTable.getColumns().add(Cmimic);
+		LinjattablesTable.getColumns().add(EmriKompanisc);
+		LinjattablesTable.getColumns().add(Verifikmic);
+	
+		
+		LinjattablesTable.setPrefWidth(650);
+		LinjattablesTable.setPrefHeight(200);
+		
+		// Main Pane
+		VBox mainPane = new VBox(10);
+		
+		mainPane.getChildren().addAll(txtlinjat,LinjattablesTable,downPane);
+		
+		mainPane.setPadding(new Insets(15, 15, 15 ,15));
 		
 		
-		Scene linjatscene = new Scene(linjatpane,700,500);
-		linjat.setScene(linjatscene);
-		linjat.show();
-	    
-	   
+		Scene scene = new Scene(mainPane,550, 400);
 		
+		primaryStage.setTitle("Library");
+		primaryStage.setScene(scene);
 		
+		showLinjattables();
+		
+		primaryStage.show();
 	}
 	
-	
-	 public static List<LinjatA> getLinjatA() {
-		 
-		 DbConnection.getConnection();
-		 
-	    	List<LinjatA> ListaL = new ArrayList();
-	    	
-	    	String query ="Select * from Linjat";
-	    	try {
-	    		
-	    		PreparedStatement prepareStatement = DbConnection.getConnection().prepareStatement(query);
-	    		ResultSet resulset =prepareStatement.executeQuery();
-	    		
-	    		while(resulset.next())
-	    		{
-	    			LinjatA linjat = new LinjatA
-	    				(   resulset.getInt(1) ,
-	    					resulset.getString(2),
-	    					resulset.getString(3),
-	    					resulset.getTime(4),
-	    					resulset.getTime(5),
-	    					resulset.getFloat(6),
-	    					resulset.getString(7),
-	    					resulset.getBoolean(9));
-	    			
-	    			ListaL.add(linjat);
-	    		}
-	    			
-	    		
-	    	}catch(SQLException e)
-	    	{
-	    		e.printStackTrace();
-	    		
-	    	}
-	    	
-			return null;
-			
-	    }
 
-
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 	
-}
+	public void showLinjattables() {
+		List<Linjattable> Linjattables = Linjattable.getLinjattables();
+		
+		ObservableList<Linjattable> LinjattableList = FXCollections.observableArrayList();
+		
+		for(int i = 0; i < Linjattables.size(); i++) {
+			LinjattableList.add(Linjattables.get(i));
+		}
+		
+		LinjattablesTable.setItems(LinjattableList);
+	}
+	
+	}
+	
