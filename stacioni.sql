5c65ac60d6a9ac532de5abeab291dffef5f22a92
Vetonshala <43969779+vetonnshala@users.noreply.github.com>
2019-06-06 00:45:35 +0200

    Update stacioni.sql

--- stacioni.sql
+++ stacioni.sql
@@ -1,7 +1,8 @@
 create database Stacioni;
 use Stacioni;
-drop database stacioni;
 
+
+insert into Autobusi(EmriAutobusit ,Pronari,Numri_Telefonit,Lokacioni,Nrudhetarve)values('Genti Tours','Ali Bytyqi',045856958,'Prishtine',80);      
 create table Autobusi (
 Aid integer primary key auto_increment not null  ,
 EmriAutobusit varchar(30) not null ,
@@ -11,7 +12,7 @@ Lokacioni varchar(40),
 Nrudhetarve integer not null 
 );
 
-select * from Linjat;
+
 create table Linjat (
 Lid integer primary key auto_increment not null  ,
 Vendi_nisjes varchar(50) not null,
@@ -20,8 +21,12 @@ Oranisjes time not null ,
 OraArritjes time not null ,
 Cmimi real not null ,
 EmriKompanis varchar(30) ,
-Verifikmi boolean);
-                         						
+Verifikmi boolean
+
+); 
+
+ insert into Linjat(Vendi_nisjes,Destinacioni,Oranisjes,OraArritjes,Cmimi,EmriKompanis,Verifikmi)
+ values('Prishtine','Lipjan','08:00:00','08:30:00',00.50,'Ali',1);                        						
 
 
 create table Biletat (
@@ -33,9 +38,7 @@ B_ora time not null ,
 NrBiletav integer not null ,
 Kthyese boolean,
 foreign key(Lid) references Linjat(Lid) on delete cascade);
-
-
-
+                   						
 create table Regjistrohu(
 User_id integer primary key  auto_increment not null ,
 Emri varchar(30) not null ,
@@ -45,7 +48,7 @@ Username varchar(40) not null ,
 Gjinia varchar(20),
 Password varchar(40) not null);
 
-
+insert into Regjistrohu(Emri,Mbiemri,Email,Username,Gjinia,Password) values ('Veton','Shala','vetonnshala@gmail.com','veton','M','1234');
 
 create table Komentet(
 Kid integer primary key auto_increment not null unique ,
